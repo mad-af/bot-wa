228472bcd28a51b4e3540eaecf7ce89b687499c6
azariaberyl <beryl.naga@gmail.com>
2022-08-30 12:49:04 +0700

    able to reply with image

--- coba.js
+++ coba.js
@@ -2,7 +2,7 @@
 const sheetApi = require("./sheet.js");
 const qrcode = require('qrcode-terminal');
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
 let availableNumbers = [] 
 const getAvailableNumbers = async () => {
@@ -46,17 +46,24 @@ for (let e of ['one']) {
   client.on('message', async (message) => {
     const name = message._data.notifyName;
     const number = message.from.replace('@c.us','');
+    // console.log(message);
+
     if(!isNumberExist(number)){
       (await sheetApi()).setRows(name, number);
       console.log('Berhasil menambahkan nomer');
     }
-    console.log(availableNumbers);
+    // console.log(availableNumbers);
     // console.log(name, number);
 
     const rows = (await sheetApi()).getRows;
     for (const row of rows) {
       if(message.body === row[0]) {
         message.reply(''+row[1]);
+      } 
+      else if (message.body === '!image') {
+        const media = await MessageMedia.fromUrl('https://www.99.co/blog/indonesia/wp-content/uploads/2021/12/contoh-gambar-fauna-yang-mudah.jpg')
+        client.sendMessage(message.from, media);
+        break;
       }
     }
     getAvailableNumbers();
